7f060f34e29ff26c5f40c0c8557d00c1b58b6968
Augusto94-coder <augustodileo94@gmail.com>
2025-10-30 17:35:50 +0100

    SHOW singolo film

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -15,7 +15,20 @@ function index(req, res) {
 
 //  SHOW
 function show(req, res) {
-    console.log("hai richiesto la rotta show");
+    const id = req.params.id;
+
+    // prepariamo query per singolo libro
+    const movieSql = 'SELECT * FROM movies WHERE id = ?';
+
+    // aggiungiamo la connesione per la richiesta
+    connection.query(movieSql, [id], (err, movieResult) => {
+        // gestiamo errore server mysql
+        if (err) return res.status(500).json({ error: "Database error" })
+        // gestiamo anche il 404
+        if (movieResult.length === 0) res.status(404).json({ error: "Book not found" })
+        // ritorniamo il risultato ottenuto
+        res.json(movieResult[0]);
+    });
 }
 
 module.exports = { index, show }
